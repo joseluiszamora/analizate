1fcea514c2d1d11f3658902d7763b5d2c4f1f837
Elmer Mendoza <defreddyelmer@gmail.com>
2013-08-07 23:48:51 -0400

    Create default user in seed

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,6 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable, :token_authenticatable,
-         :recoverable, :rememberable, :trackable, :validatable, :confirmable,
+         :recoverable, :rememberable, :trackable, :validatable, #:confirmable,
          :timeoutable
 end
