943fb666f1099dcd55c6928261ead1238275918b
Elmer Mendoza <defreddyelmer@gmail.com>
2013-08-07 23:33:54 -0400

    Install devise

---
+++ app/models/user.rb
@@ -0,0 +1,7 @@
+class User < ActiveRecord::Base
+  # Include default devise modules. Others available are:
+  # :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable, :token_authenticatable,
+         :recoverable, :rememberable, :trackable, :validatable, :confirmable,
+         :timeoutable
+end
