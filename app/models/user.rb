e86ad232eaad1b7445e259e0bb97f6a039f4f0b4
Elmer Mendoza <defreddyelmer@gmail.com>
2013-08-08 00:06:27 -0400

    Enable auth_token for users

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ActiveRecord::Base
+  before_save :ensure_authentication_token
   # Include default devise modules. Others available are:
   # :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable, :token_authenticatable,
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Analizate::Application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: { sessions: 'sessions' }
 
   root 'welcome#index'
 end
