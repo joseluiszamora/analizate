9402ad779f66c9c1cb5bc73b921fcdbb00832df1
Elmer Mendoza <defreddyelmer@gmail.com>
2013-08-31 12:16:31 -0400

    Edit form analysis

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,14 @@
 module ApplicationHelper
+  def check_category(analysis, category)
+    ac = analysis.categories.first
+    ac.present? && ac.parent_id == category.id
+  end
+
+  def check_subcategory(analysis, subcategory)
+    sc = analysis.categories.first
+    sc.present? && sc.id == subcategory.id
+  end
+
   def get_categories
     TestCategory.roots
   end
