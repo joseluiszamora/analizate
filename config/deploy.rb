7aac7efe16378882618d26e53290f706b775e928
Jose Luis Zamora <jzamora@geo.gob.bo>
2013-09-20 14:52:24 -0400

    deploy do

--- config/deploy.rb
+++ config/deploy.rb
@@ -44,8 +44,8 @@ namespace :deploy do
 
   desc "Make sure local git is in sync with remote."
   task :check_revision, roles: :web do
-    unless `git rev-parse HEAD` == `git rev-parse origin/master`
-      puts "WARNING: HEAD is not the same as origin/master"
+    unless `git rev-parse HEAD` == `git rev-parse origin/develop`
+      puts "WARNING: HEAD is not the same as origin/develop"
       puts "Run `git push` to sync changes."
       exit
     end
