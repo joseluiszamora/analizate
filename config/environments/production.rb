4a4b435402deb229936826c090d58586da064e1d
Jose Luis Zamora <jzamora@geo.gob.bo>
2013-09-23 14:37:19 -0400

    frontend styles

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -59,7 +59,7 @@ Analizate::Application.configure do
 
   # Precompile additional assets.
   # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
-  config.assets.precompile += %w( login.js login.css )
+  config.assets.precompile += %w( login.js login.css frontend.js frontend.css )
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
