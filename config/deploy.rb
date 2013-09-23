6e553707962ed668a84a40e4aa2a16b01b931bbd
Jose Luis Zamora <jzamora@geo.gob.bo>
2013-09-23 14:48:18 -0400

    change uploads folder

--- config/deploy.rb
+++ config/deploy.rb
@@ -39,7 +39,7 @@ namespace :deploy do
 
   task :symlink_config, roles: :app do
     run "ln -nfs #{shared_path}/config/database.yml #{release_path}/config/database.yml"
-    run "ln -nfs #{shared_path}/public/uploads #{release_path}/public/uploads"
+    run "ln -nfs #{shared_path}/public/uploads #{release_path}/public"
   end
   after "deploy:finalize_update", "deploy:symlink_config"
 
