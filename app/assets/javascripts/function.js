71c8f39e508347b0c9080caf1b6f81323185548b
Jose Luis Zamora <jzamora@geo.gob.bo>
2013-09-26 19:27:32 -0400

    test new

--- app/assets/javascripts/function.js
+++ app/assets/javascripts/function.js
@@ -1,9 +1,15 @@
 $(document).ready(function(){
   $(".datepicker").datepicker({
     format: "dd/mm/yyyy"
-  }).on("changeDate", function(ev) {
+  }).on("changeDate", function() {
     return $(this).datepicker("hide");
   });
+
+  // $("#user-datepicker").datepicker({
+  //   format: "dd/mm/yyyy"
+  // }).on("changeDate", function(ev) {
+  //   return $(this).datepicker("hide");
+  // });
 
   $(".timepicker").timepicker({
     showMeridian: false
