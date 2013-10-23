218791e0c2ab55e9bf169d5da834c4217d921fc3
Elmer Mendoza <defreddyelmer@gmail.com>
2013-10-23 14:39:32 -0400

    Update deploy file

--- config/deploy.rb
+++ config/deploy.rb
@@ -18,7 +18,10 @@ set :branch, "develop"
 default_run_options[:pty] = true
 ssh_options[:forward_agent] = true
 
+after "deploy", "deploy:stop"
+after "deploy", "deploy:start"
 after "deploy", "deploy:cleanup" # keep only the last 5 releases
+after "deploy:update_code", "deploy:migrate"
 
 namespace :deploy do
   %w[start stop restart].each do |command|
