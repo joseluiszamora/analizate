78458318938931f09172f9a040f8e8f2998d3f84
Joseph <joseluiszamorag@gmail.com>
2014-01-25 08:42:12 -0800

    quienes somos

--- db/schema.rb
+++ db/schema.rb
@@ -26,8 +26,8 @@ ActiveRecord::Schema.define(version: 20131230145453) do
     t.boolean  "patient_type_selector", default: false
   end
 
-  add_index "analyses", ["doctor_id"], name: "index_analyses_on_doctor_id", using: :btree
-  add_index "analyses", ["patient_id"], name: "index_analyses_on_patient_id", using: :btree
+  add_index "analyses", ["doctor_id"], name: "index_analyses_on_doctor_id"
+  add_index "analyses", ["patient_id"], name: "index_analyses_on_patient_id"
 
   create_table "institutions", force: true do |t|
     t.string   "category"
@@ -52,8 +52,8 @@ ActiveRecord::Schema.define(version: 20131230145453) do
     t.string   "image"
   end
 
-  add_index "laboratories", ["analysis_id"], name: "index_laboratories_on_analysis_id", using: :btree
-  add_index "laboratories", ["test_id"], name: "index_laboratories_on_test_id", using: :btree
+  add_index "laboratories", ["analysis_id"], name: "index_laboratories_on_analysis_id"
+  add_index "laboratories", ["test_id"], name: "index_laboratories_on_test_id"
 
   create_table "notices", force: true do |t|
     t.string   "title"
@@ -87,12 +87,12 @@ ActiveRecord::Schema.define(version: 20131230145453) do
     t.integer  "level"
   end
 
-  add_index "test_categories", ["ancestry"], name: "index_test_categories_on_ancestry", using: :btree
+  add_index "test_categories", ["ancestry"], name: "index_test_categories_on_ancestry"
 
   create_table "tests", force: true do |t|
     t.string   "parameter"
     t.string   "unit"
-    t.text     "reference_values"
+    t.text     "reference_values", limit: 255
     t.string   "test_type"
     t.text     "description"
     t.string   "image"
@@ -102,7 +102,7 @@ ActiveRecord::Schema.define(version: 20131230145453) do
     t.integer  "level"
   end
 
-  add_index "tests", ["test_category_id"], name: "index_tests_on_test_category_id", using: :btree
+  add_index "tests", ["test_category_id"], name: "index_tests_on_test_category_id"
 
   create_table "users", force: true do |t|
     t.string   "email",                  default: "", null: false
@@ -134,18 +134,18 @@ ActiveRecord::Schema.define(version: 20131230145453) do
     t.string   "cellular"
     t.date     "birthday"
     t.text     "notes"
-    t.string   "image"
     t.string   "role"
-    t.integer  "specialty_id"
+    t.string   "image"
     t.string   "especiality"
+    t.integer  "specialty_id"
     t.integer  "level"
   end
 
-  add_index "users", ["authentication_token"], name: "index_users_on_authentication_token", unique: true, using: :btree
-  add_index "users", ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true, using: :btree
-  add_index "users", ["email"], name: "index_users_on_email", using: :btree
-  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
-  add_index "users", ["unlock_token"], name: "index_users_on_unlock_token", unique: true, using: :btree
-  add_index "users", ["username"], name: "index_users_on_username", unique: true, using: :btree
+  add_index "users", ["authentication_token"], name: "index_users_on_authentication_token", unique: true
+  add_index "users", ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
+  add_index "users", ["email"], name: "index_users_on_email"
+  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  add_index "users", ["unlock_token"], name: "index_users_on_unlock_token", unique: true
+  add_index "users", ["username"], name: "index_users_on_username", unique: true
 
 end
